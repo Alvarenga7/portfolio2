379925784f4411a4c479a4f9c37a47630e96ceb2
Henrique Hastenreiter <109881935+Alvarenga7@users.noreply.github.com>
2024-12-27 17:30:45 -0300

    Update intro-section.tsx

--- src/app/componentes/intro-section.tsx
+++ src/app/componentes/intro-section.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./mode-toggle";
 
 function IntroSection(){
     return(
-        <section id="intro" className="space-y-6 py-8 md:py-12 lg:py-32 relative">
+        <section id="intro" className="space-y-6 py-8 md:py-12 lg:py-32 relative xs:ml-[-25%] md:ml-[1%]">
         <svg viewBox="0 0 1024 1024" className=" absolute left-1/3 top-full md:left-1/2 md:top-1/2 h-[16rem] w-[16rem] md:h-[54rem] md:w-[54rem] size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
           <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
           <defs>
@@ -19,7 +19,7 @@ function IntroSection(){
               
               <ModeToggle />
             </span>
-            <h1 className="font-heading text-6xl md:text-7xl lg:text-7xl">
+            <h1 className="font-heading text-6xl md:text-7xl lg:text-7xl ">
               FRONT-END
               <br />
               <span className="text-primary text-7xl">DEVELOPER</span>
